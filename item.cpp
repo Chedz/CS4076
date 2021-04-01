57f1b31b6c16e8be63618ee785b4aadf3c5f0cc4
nathangold <64435663+nathangold@users.noreply.github.com>
2021-04-01 17:50:18 +0100

    Update item.cpp

--- item.cpp
+++ item.cpp
@@ -1,10 +1,9 @@
 #include "item.h"
 
-Item::Item (string inDescription, int inWeightGrams, float inValue/**, int weaponCheck*/) {
+Item::Item (string inDescription, int inWeightGrams) {
 	description = inDescription;
 	setWeight(inWeightGrams);
-	value = inValue;
-	/**weaponCheck(isWeapon);*/
+
 }
 
 Item::Item(string inDescription) {
@@ -19,21 +18,6 @@ void Item::setWeight(int inWeightGrams)
 	   weightGrams = inWeightGrams;
 }
 
-void Item::setValue(float inValue)
-{
-    if (inValue > 9999 || inValue < 0)
-       cout << "value invalid, must be 0<value<9999" ;
-    else
-	   value = inValue;
-}
-
-/**void Item::setWeaponCheck(int isWeapon)
-{
-    if(isWeapon > 0 || isWeapon < 0)
-        cout << "Item not a weapon" ;
-    else
-        cout << "Item is a weapon" ;
-}*/
 
 string Item::getShortDescription()
 {
