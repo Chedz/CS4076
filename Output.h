131cb79204528d9bded5177c213aa3317b893bf2
nathangold <64435663+nathangold@users.noreply.github.com>
2021-04-02 22:27:01 +0100

    Update Output.h

--- Output.h
+++ Output.h
@@ -1,1 +1,24 @@
 
+#ifndef OUTPUT_H_
+#define OUTPUT_H_
+
+
+#include <string>
+#include <vector>
+#include "ZorkUL.h"
+
+using namespace std;
+using std::vector;
+
+class Output {
+
+private:
+	
+	vector<string> textBlocks;
+    
+  public :
+  
+  void typePrint(string text);
+};
+
+#endif
